545415dae43dc49625895f59854281472b5e18df
Prawirdani <prawirdani@icloud.com>
2024-03-11 22:56:11 +0700

    add diagnostics popup

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -16,3 +16,16 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Diagnostic Config
+vim.o.completeopt = "menuone,noinsert,noselect"
+vim.opt.shortmess = vim.opt.shortmess + "c"
+
+vim.diagnostic.config({
+	virtual_text = false,
+	float = { border = "rounded" },
+})
+
+vim.o.updatetime = 750
+vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
+vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335 guifg=#abb2bf]])
