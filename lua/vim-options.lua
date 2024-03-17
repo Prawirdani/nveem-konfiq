ef03b8c555c98ad1f128b65e9c2836bcaaa27ace
Prawirdani <prawirdani@icloud.com>
2024-03-18 06:27:26 +0700

    add keybinding for exit term mode

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -30,4 +30,5 @@ vim.o.updatetime = 750
 vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
 vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335 guifg=#abb2bf]])
 
+-- Term Normal Mode Keymap
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><c-n>", { noremap = true, desc = "Term back to normal mode" })
