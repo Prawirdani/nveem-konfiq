8919ba93f884ff3f852a73ab25e63d8354a94657
Prawirdani <prawirdani@gmail.com>
2024-05-02 22:43:50 +0700

    rm eslint_d

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -74,6 +74,7 @@ return {
 			lspconfig.svelte.setup({ capabilities = capabilities })
 			-- SQL LS
 			lspconfig.sqlls.setup({ capabilities = capabilities })
+			lspconfig.intelephense.setup({ capabilities = capabilities })
 			-- HTML LS
 			lspconfig.html.setup({ capabilities = capabilities })
 			lspconfig.emmet_language_server.setup({ capabilities = capabilities })
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -13,7 +13,6 @@ return {
 				-- Lua
 				b.formatting.stylua,
 				-- JS/TS
-				require("none-ls.diagnostics.eslint_d"),
 				b.formatting.prettier.with({
 					filetypes = {
 						"html",
@@ -30,6 +29,7 @@ return {
 				b.formatting.goimports,
 				-- Python
 				b.formatting.black,
+				b.formatting.pretty_php,
 				require("none-ls.diagnostics.ruff"),
 			},
 			on_attach = function(client, bufnr)
